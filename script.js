61d48a8ab7d397fde44ed391c02781ab3123b477
joaovitorangelo <joaovitorangelo05@gmail.com>
2023-09-03 03:38:18 -0300

    add modal

--- script.js
+++ script.js
@@ -23,3 +23,16 @@ function toggleMode() {
     }
 }
 
+//modal
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-mod");
+const modal = document.querySelector("#mod");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    [modal, fade]. forEach((el) => el.classList.toggle("hide"));
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
